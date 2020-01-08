acc5c946967290e3fedf773a577426279c0b0692
Sam W <sam@sam-we.com>
2020-01-07 19:12:02 -0800

    Improve tests

--- tests-2.sh
+++ tests-2.sh
@@ -2,7 +2,7 @@
 
 @test 'returns 0' {
   source insist_on_latest_ruby
-  run RBENV_VERSION="2.7.0" insist_on_latest_ruby
+  run insist_on_latest_ruby
   [ "$status" -eq 0 ]
 }
 
--- tests.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bats
-
-@test 'returns 1' {
-  source insist_on_latest_ruby
-  run insist_on_latest_ruby
-  [ "$status" -eq 1 ]
-}
-
