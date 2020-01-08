c88ba4a2a1e9941b9850f5ba13f91b3564891259
Sam W <sam@sam-we.com>
2020-01-07 18:55:53 -0800

    Improve tests

--- tests.sh
+++ tests.sh
@@ -2,9 +2,7 @@
 
 @test 'returns 1' {
   source insist_on_latest_ruby
-  run rvm 2.0 do insist_on_latest_ruby
+  run insist_on_latest_ruby
   [ "$status" -eq 1 ]
-  run rvm 2.7 do insist_on_latest_ruby
-  [ "$status" -eq 0 ]
 }
 
